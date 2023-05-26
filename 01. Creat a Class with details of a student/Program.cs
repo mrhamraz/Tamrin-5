fc3c345ecf914e1b42127d43d71348c8c5810132
Hamraz <x6.reza@gmail.com>
2023-05-26 16:17:05 +0330

    Add project files.

---
+++ 01. Creat a Class with details of a student/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Creat_a_Class_with_details_of_a_student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
