0a0c4d2c5f23e3ad4859d0fd9dff49334c2debcd
Hamraz <x6.reza@gmail.com>
2023-05-30 22:04:54 +0330

    5.4

---
+++ 04. Teacher class with constructor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04._Teacher_class_with_constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name of teacher:");
+
+            Teacher teacher = new Teacher(Console.ReadLine());
+
+            teacher.Print();
+        }
+
+        class Teacher
+        {
+            public string name;
+            public string specialty;
+            public int salary;
+
+            public Teacher(string Name)
+            {
+                name = Name;
+                specialty = "default";
+                salary = 0;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"\nName: {name} \nspecialty: {specialty} \nSalary: {salary} ");
+            }
+        }
+    }
+}
