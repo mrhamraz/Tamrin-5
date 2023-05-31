cb7eac504f46eb4ccc9a0e9f789fc65a4a67ee28
Hamraz <x6.reza@gmail.com>
2023-05-31 17:12:19 +0330

    5.6

---
+++ 06. Print student informations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06._Print_student_informations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = {"ali","mammad","hasan" }; 
+
+            /*
+              for (int i = 0; i < students.Length; i++)
+                Student student[i] = new Student();
+            */
+        }
+
+        class Student
+        {
+            public string firstName;
+            public string lastName;
+            public int age;
+            public double grade;
+            public string schoolName;
+
+            public void Print()
+            {
+                Console.WriteLine($"\nName: {firstName} \nLast Name: {lastName} \nAge: {age} \nGrade: {grade} \nSchool Name: {schoolName} ");
+            }
+        }
+
+    }
+}
